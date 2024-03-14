31ee94a34474f7f50919c8bb7e8a0d49c1ff8a78
peter staal <peterstaal@MBP-van-peter.home>
2024-03-14 09:47:05 +0100

    exercise 5.12

--- part2/bloglist-frontend-main/src/components/Blog.jsx
+++ part2/bloglist-frontend-main/src/components/Blog.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 
 const Blog = ({ blog, updateBlog, removeBlog }) => {
   const [visible, setVisible] = useState(false);
@@ -50,6 +51,12 @@ const Blog = ({ blog, updateBlog, removeBlog }) => {
       </div>
     </div>
   );
+};
+
+Blog.propTypes = {
+  blog: PropTypes.object.isRequired,
+  updateBlog: PropTypes.func.isRequired,
+  removeBlog: PropTypes.func.isRequired,
 };
 
 export default Blog;
